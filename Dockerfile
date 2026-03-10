1190f6e33ec4316ee23fbd684082303f19aecdfa
Masha Nesterova <mashanesterova@Mac.localdomain>
2026-03-10 17:03:42 +0200

    Додала тести для магазину

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3.11-slim
 WORKDIR /app
-RUN pip install django
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+class ShopTest(TestCase):
+    def setUp(self):
+        # Створюємо пробний товар для тесту
+        Product.objects.create(name="Тестовий подарунок", price=500, description="Дуже гарний")
+
+    def test_product_content(self):
+        # Перевіряємо, чи товар дійсно з'явився в базі з правильною ціною
+        item = Product.objects.get(id=1)
+        self.assertEqual(item.name, "Тестовий подарунок")
+        self.assertEqual(item.price, 500)
